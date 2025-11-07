d4ca10b31605744323d7ae9172630671d7dcc75b
Ofallbrains <ofallbrains@gmail.com>
2025-11-07 13:55:03 +0200

    Route Handling

---
+++ step-02-http-server/routingServer.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain')
+
+    switch (req.url) {
+        case '/':
+            res.statusCode = 200
+            res.end('Home Page')
+            break;
+        case '/about':
+            res.statusCode = 200
+            res.end('About Page')
+            break;
+        case '/contact':
+            res.statusCode = 200
+            res.end('Contact Page')
+            break;
+    
+        default:
+            res.statusCode = 404;
+            res.end('Unkown route')
+            break;
+    }
+})
+
+server.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
