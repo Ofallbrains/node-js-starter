a6bb0f4322589a374adc8db7b86f6f1bf439beb7
Ofallbrains <ofallbrains@gmail.com>
2025-11-11 14:03:25 +0200

    Added some changes

--- step-02-http-server/apiServer.js
+++ step-02-http-server/apiServer.js
@@ -11,9 +11,13 @@ const server = http.createServer((req, res) => {
   if (req.url === '/api/products' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(products));
-  } else {
+  } {
     res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ error: '404 - Not Found' }));
+    res.end(JSON.stringify({
+      success: false,
+      error: 'Product not found',
+      data: null
+    }));
   }
 });
 
