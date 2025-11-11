b8266dc7efa89e7f4357a8e374e0d238782e263a
Ofallbrains <ofallbrains@gmail.com>
2025-11-11 16:27:34 +0200

    Setting up midleware to parse json

---
+++ step-04-routes-middleware/03.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.use(express.json())
+
+app.post('/api/data', (req, res) => {
+    console.log(req.body);
+    res.json({message: 'Data received', data:req.body})
+    
+})
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+    
+})
