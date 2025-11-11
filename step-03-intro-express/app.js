00337b012bff6d8b0e63dd69f5f596d63ae3a6c9
Ofallbrains <ofallbrains@gmail.com>
2025-11-11 14:13:00 +0200

    Creating a basic express server

---
+++ step-03-intro-express/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello from Express!")
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    
+})
